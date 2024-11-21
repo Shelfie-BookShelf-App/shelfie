8a79c4e919a31be1017b673a9c0ea385f1211b06
Hiep Nguyen <hiepnguyenduc2005@gmail.com>
2024-11-21 00:23:47 -0500

    deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../server/public',
+    emptyOutDir: true
+  },
+  resolve: {
+    alias: {
+      'picocss': path.resolve(__dirname, '../node_modules/@picocss/pico/css')
+    }
+  },
   server: {
     proxy: {
       '/api': {
